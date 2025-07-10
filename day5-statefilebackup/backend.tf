e7e275f5c218bf35c24cee5cb56bb2f62e11182e
TAJ-GIT <mahammadtaj3666@gmail.com>
2025-07-10 11:40:26 +0530

    Update backend.tf

--- day5-statefilebackup/backend.tf
+++ day5-statefilebackup/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.10.0"
 
   backend "s3" {
-    bucket       = "qwsdfghjuytfcbju"
+    bucket       = "qwsdfghjuytfcbju"   #bucket name
     key          = "terraform.tfstate"
     region = "us-east-1"
   }
