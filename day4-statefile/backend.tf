9958a0982ffa9a847e0f6332bfaadf3593dd35b1
TAJ-GIT <mahammadtaj3666@gmail.com>
2025-07-15 23:28:17 +0530

    file

--- day4-statefile/backend.tf
+++ day4-statefile/backend.tf
@@ -1,9 +1,11 @@
 terraform {
-  required_version = ">= 1.10.0"
-
+#required_version = ">=1.10" # this will allow to work same terraform version only 
   backend "s3" {
-    bucket       = "qwsdfghjuytfcbju"   #bucket name
-    key          = "terraform.tfstate"
+    bucket = "nareshitveeradevops"
+    key    = "terraform.tfstate"
     region = "us-east-1"
+    use_lockfile = true #supports latest version >=1.10
+    #dynamodb_table = "veera"
+    #encrypt = true
   }
 }
